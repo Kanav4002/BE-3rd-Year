457dd61d10b2464660e81710c4903ce714bc8ef5
Kanav4002 <kumarkanav5753@gmail.com>
2025-08-07 11:19:44 +0530

    made functions to get all the todos appear on the screen

--- Lecture-14(Todo_App)/public/script.js
+++ Lecture-14(Todo_App)/public/script.js
@@ -1,4 +1,37 @@
 const filters = document.getElementById("filters");
+const form = document.getElementById("task-form");
+const todosContainer = document.getElementById("todos-container");
+
+getAllTodos(); // get todos and renders it when js loads
+
+async function getAllTodos() {
+  const res = await axios.get("http://localhost:4000/todo/all");
+  const todos = res.data.todos;
+  renderTools(todos);
+}
+
+function renderTools(todos) {
+  for (let todo of todos) {
+    const div = document.createElement("div");
+    div.className = "todo";
+    div.innerHTML = `<h3>${todo.task}</h3>
+    <div>
+      <button class="status">${todo.status ? "Undo" : "Completed"}</button>
+      <button class="delete">Delete</button>
+    </div>`
+    todosContainer.prepend(div);
+  }
+}
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // to stop page refresh on submit
+  const input = form.children[0];
+  const task = input.value;
+  const res = await axios.post("http://localhost:4000/todo/create", {
+    task: task
+  });
+
+})
 
 filters.addEventListener("click", (e) => {
   const btnId = e.target.id;
